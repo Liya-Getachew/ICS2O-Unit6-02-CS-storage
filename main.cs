9eefa0554d52171ca0c6d4ebac5f38a215661f19
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-12-14 02:30:20 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,47 @@
+// Created by: liya getachew
+// Created on: Dec 13 2022
+//
+// This program accepts user input
+
+using System;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program
+{
+    public static async Task Main()
+    {
+        string response;
+        int counter = 0;
+
+        Console.WriteLine("Would you like to quit?");
+        Console.WriteLine("");
+
+        Console.Write("yes or no: ");
+        response = Console.ReadLine();
+        Console.WriteLine("");
+
+        while (response == "no")
+        {
+            counter++;
+            Console.WriteLine("counter = " + counter);
+
+            await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
+            Console.WriteLine("File created...");
+            Console.WriteLine("");
+
+            var someText = await File.ReadAllTextAsync(@"WriteText.txt");
+
+            Console.WriteLine("Would you like to quit?");
+            Console.WriteLine("");
+
+            Console.Write("yes or no: ");
+            response = Console.ReadLine();
+            Console.WriteLine("");
+        }
+
+        counter = 0;
+
+        Console.WriteLine("\nDone.");
+  }
+}
